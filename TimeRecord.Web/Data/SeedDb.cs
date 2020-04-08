35cacc1b7f9b86a44ae8b666d2464804e3d5b34a
CARLOS FELIPE OQUENDO ESCOBAR <carlosoquendo2@hotmail.com>
2020-04-07 21:38:12 -0500

    Change SeedDb

--- TimeRecord.Web/Data/SeedDb.cs
+++ TimeRecord.Web/Data/SeedDb.cs
@@ -23,14 +23,17 @@ namespace TimeRecord.Web.Data
         {
             await _context.Database.EnsureCreatedAsync();
             await CheckRolesAsync();
+            await CheckExpenseTypesAsync();
             await CheckUserAsync("1010", "Carlos", "Oquendo", "carlosoquendo2@hotmail.com", "3010000000", "Avenida siempre viva", UserType.Admin);
-            await CheckUserAsync("1010", "Felipe", "Escobar", "carlosoquendo1206@gmail.com", "3010000000", "Avenida siempre viva", UserType.User);
+            await CheckUserAsync("1011", "Felipe", "Escobar", "carlosoquendo1206@gmail.com", "3010000000", "Avenida siempre viva", UserType.User);
+            await CheckUserAsync("1012", "Mario", "Bedoya", "mario.bedoya@yopmail.com", "3010000000", "Avenida siempre viva", UserType.User);
+            await CheckUserAsync("1013", "Catalina", "Velez", "catalina.velez@yopmail.com", "3010000000", "Avenida siempre viva", UserType.User);
             await CheckUsersAsync();
         }
 
         private async Task CheckUsersAsync()
         {
-            for (int i = 1; i <= 100; i++)
+            for (int i = 1; i <= 20; i++)
             {
                 await CheckUserAsync($"100{i}", "User", $"{i}", $"user{i}@yopmail.com", "350 634 2747", "Calle Luna Calle Sol", UserType.User);
             }
@@ -65,9 +68,73 @@ namespace TimeRecord.Web.Data
 
                 var token = await _userHelper.GenerateEmailConfirmationTokenAsync(user);
                 await _userHelper.ConfirmEmailAsync(user, token);
+
+                if (user.UserType == UserType.User)
+                {
+                    await CheckTrips(user);
+                }
             }
 
             return user;
+        }
+
+        private async Task CheckTrips(UserEntity user)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                await CreateTrip(user);
+            }
+        }
+
+        private async Task CreateTrip(UserEntity user)
+        {
+            _context.Trips.Add(new TripEntity
+            {
+                Name = "Trip example - " + user.LastName,
+                Description = "Test description",
+                StartDate = DateTime.Today.ToUniversalTime(),
+                EndDate = DateTime.Today.AddMonths(1).ToUniversalTime(),
+                User = user,
+                TripDetails = new List<TripDetailEntity>
+                {
+                    new TripDetailEntity
+                    {
+                        ExpenseType = _context.ExpenseTypes.FirstOrDefault(),
+                        Name = "Trip detail 1",
+                        Expense = 9.00,
+                        Currency = CurrencyType.USD.ToString(),
+                        Comment = "Trip detail comment 1",
+                        Date = DateTime.Today.ToUniversalTime()
+                    },
+                    new TripDetailEntity
+                    {
+                        ExpenseType = _context.ExpenseTypes.Last(),
+                        Name = "Trip detail 2",
+                        Expense = 15.50,
+                        Currency = CurrencyType.USD.ToString(),
+                        Comment = "Trip detail comment 2",
+                        Date = DateTime.Today.ToUniversalTime()
+                    },
+                    new TripDetailEntity
+                    {
+                        ExpenseType = _context.ExpenseTypes.FirstOrDefault(),
+                        Name = "Trip detail 3",
+                        Expense = 10.20,
+                        Currency = CurrencyType.USD.ToString(),
+                        Comment = "Trip detail comment 3",
+                        Date = DateTime.Today.ToUniversalTime()
+                    }
+                }
+            });
+            await _context.SaveChangesAsync();
+        }
+
+        private async Task CheckExpenseTypesAsync()
+        {
+            _context.ExpenseTypes.Add(new ExpenseTypeEntity { Name = "Taxi", Active = true });
+            _context.ExpenseTypes.Add(new ExpenseTypeEntity { Name = "Breakfast", Active = true });
+            _context.ExpenseTypes.Add(new ExpenseTypeEntity { Name = "Dinner", Active = true });
+            await _context.SaveChangesAsync();
         }
 
         private async Task CheckRolesAsync()
