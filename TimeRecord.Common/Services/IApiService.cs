d5262511990d86f10611df2401acb87dcf89ac52
CARLOS FELIPE OQUENDO ESCOBAR <carlosoquendo2@hotmail.com>
2020-04-12 16:53:37 -0500

    Revert "Revert "Add trip page Xamarin""
    This reverts commit 71098e373e4937b0ce12994e12d337596d9e135c.

---
+++ TimeRecord.Common/Services/IApiService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using TimeRecord.Common.Models;
+
+namespace TimeRecord.Common.Services
+{
+    public interface IApiService
+    {
+        Task<Response> GetListAsync<T>(string urlBase, string servicePrefix, string controller);
+    }
+}
