208cb2133223eda841f826cacec3ebb0212f4c43
CARLOS FELIPE OQUENDO ESCOBAR <carlosoquendo2@hotmail.com>
2020-04-18 14:17:22 -0500

    Add multilanguage

---
+++ TimeRecord.Prism/TimeRecord.Prism/Helpers/Languages.cs
@@ -0,0 +1,22 @@
+﻿using TimeRecord.Prism.Resources;
+using System.Globalization;
+using Xamarin.Forms;
+using TimeRecord.Common.Interfaces;
+
+namespace TimeRecord.Prism.Helpers
+{
+    public static class Languages
+    {
+        static Languages()
+        {
+            CultureInfo ci = DependencyService.Get<ILocalize>().GetCurrentCultureInfo();
+            Resource.Culture = ci;
+            Culture = ci.Name;
+            DependencyService.Get<ILocalize>().SetLocale(ci);
+        }
+
+        public static string Culture { get; set; }
+
+        public static string Error => Resource.Error;
+    }
+}
